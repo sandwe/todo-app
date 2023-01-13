7b04317e45db34478d7bb18f6e3bbb5d16306316
sandwe <preengus@gmail.com>
2023-01-13 18:27:13 +0900

    feat: react-router-dom 패키치 설치 및 라우트 설정

---
+++ client/src/router/Router.tsx
@@ -0,0 +1,22 @@
+import {Routes, Route} from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import SignupPage from "../pages/SignupPage";
+import NotFoundPage from "../pages/NotFoundPage";
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/">
+        <Route index element={<HomePage />} />
+        <Route path=":id" element={<HomePage />} />
+        <Route path="auth" element={<LoginPage />} />
+        <Route path="signup" element={<SignupPage />} />
+      </Route>
+
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+};
+
+export default Router;
