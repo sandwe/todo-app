879b9acb9b3ea653e805507a083d8c2f3de6ac84
sandwe <preengus@gmail.com>
2023-01-17 10:31:57 +0900

    feat: GlobalStyle 및 theme 설정

---
+++ client/src/styles/GlobalStyle.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  .sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    clip-path: polygon(0 0, 0 0, 0, 0);
+  } 
+
+  * {
+    box-sizing: border-box;
+  }
+
+  a {
+    text-decoration:none;
+    cursor: pointer;
+    color: inherit;
+  }
+
+  button {
+    border: none;
+    cursor: pointer;
+    padding: 0;
+    background-color: inherit;
+  }
+`;
+
+export default GlobalStyle;
---
+++ client/src/styles/theme.ts
@@ -0,0 +1,6 @@
+const theme = {
+  red: '#fd8a8a',
+  blue: '#4b56d2',
+};
+
+export default theme;
