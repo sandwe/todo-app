57e14bf776a82078d8091f225c743fc73a8590b7
sandwe <preengus@gmail.com>
2023-01-29 19:06:43 +0900

    design: 투두 리스트 디자인 추가

--- client/src/components/todos/TodoList/index.tsx
+++ client/src/components/todos/TodoList/index.tsx
@@ -1,7 +1,34 @@
-import { Container } from './style';
+import { Container, Text, Todos, Todo, Title, Content } from './style';
 
 const TodoList = () => {
-  return <Container>TodoList</Container>;
+  return (
+    <Container>
+      <h3 className='sr-only'>할 일 목록</h3>
+      <Text>Tasks 3</Text>
+      <Todos>
+        <Todo>
+          <Title>제목1</Title>
+          <Content>내용1</Content>
+        </Todo>
+        <Todo>
+          <Title>제목2</Title>
+          <Content>내용2</Content>
+        </Todo>
+        <Todo>
+          <Title>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati officiis nulla fugit ratione natus libero
+            recusandae quis aliquam, atque maxime, reprehenderit repellendus quo molestiae optio quaerat a nobis
+            deleniti praesentium.
+          </Title>
+          <Content>
+            모든 국민은 법률이 정하는 바에 의하여 선거권을 가진다. 근로조건의 기준은 인간의 존엄성을 보장하도록 법률로
+            정한다. 정부는 회계연도마다 예산안을 편성하여 회계연도 개시 90일전까지 국회에 제출하고, 국회는 회계연도 개시
+            30일전까지 이를 의결하여야 한다.
+          </Content>
+        </Todo>
+      </Todos>
+    </Container>
+  );
 };
 
 export default TodoList;
--- client/src/components/todos/TodoList/style.ts
+++ client/src/components/todos/TodoList/style.ts
@@ -1,6 +1,41 @@
 import styled from 'styled-components';
+import { ellipsisStyle } from '../../../styles/ellipsisStyle';
 
 export const Container = styled.section`
   flex-basis: 30%;
   border-right: 1px solid ${({ theme }) => theme.borderGray};
+  padding: 2rem 1rem 1rem;
+`;
+
+export const Text = styled.strong`
+  color: ${({ theme }) => theme.textGray};
+  font-size: 1.2rem;
+  margin-bottom: 1rem;
+`;
+
+export const Todos = styled.ul`
+  width: 100%;
+  border-top: 1px solid ${({ theme }) => theme.borderGray};
+  margin-top: 1rem;
+
+  & li + li {
+    border-top: 1px solid ${({ theme }) => theme.borderGray};
+  }
+`;
+
+export const Todo = styled.li`
+  padding: 1rem 0.5rem;
+`;
+
+export const Title = styled.strong`
+  font-weight: 500;
+  line-height: 1.5rem;
+  ${ellipsisStyle(1)}
+`;
+
+export const Content = styled.p`
+  font-size: 0.9rem;
+  line-height: 1rem;
+  color: ${({ theme }) => theme.textGray};
+  ${ellipsisStyle(2)}
 `;
