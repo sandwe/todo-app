28a5c316f293266a23cabd28ffbca254bd552454
sandwe <preengus@gmail.com>
2023-02-11 23:21:57 +0900

    feat: Deffered Component로 스켈레톤 UI 조건부 렌더링 #4

--- client/src/pages/TodoDetailPage/index.tsx
+++ client/src/pages/TodoDetailPage/index.tsx
@@ -1,13 +1,34 @@
+import { useState, useEffect, lazy, Suspense, PropsWithChildren } from 'react';
+
 import TodoLayout from '../../components/todos/TodoLayout';
-import { lazy, Suspense } from 'react';
 import TodoSkeleton from '../../components/todos/TodoSkeleton';
 
 const TodoDetail = lazy(() => import('../../components/todos/TodoDetail'));
 
+const DefferedComponent = ({ children }: PropsWithChildren<{}>) => {
+  const [isDeffered, setIsDeffered] = useState(false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsDeffered(true);
+    }, 200);
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  if (!isDeffered) return null;
+  return <>{children}</>;
+};
+
 const TodoDetailPage = () => {
   return (
     <TodoLayout>
-      <Suspense fallback={<TodoSkeleton />}>
+      <Suspense
+        fallback={
+          <DefferedComponent>
+            <TodoSkeleton />
+          </DefferedComponent>
+        }
+      >
         <TodoDetail />
       </Suspense>
     </TodoLayout>
