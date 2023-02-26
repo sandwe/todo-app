fde384049c11dc17f77c10cc93409d4ce973356e
sandwe <preengus@gmail.com>
2023-02-26 10:06:04 +0900

    fix: navigate를 mutate onSucess시 호출되도록 수정 #8

--- client/src/queries/auth.ts
+++ client/src/queries/auth.ts
@@ -1,18 +1,27 @@
 import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
 import authAPI from '../api/auth';
 
-export const useLoginMutation = () =>
-  useMutation({
+export const useLoginMutation = () => {
+  const navigate = useNavigate();
+
+  return useMutation({
     mutationFn: authAPI.login,
     onSuccess: (data) => {
       localStorage.setItem('token', data.token);
+      navigate('/', { replace: true });
     },
   });
+};
 
-export const useSignupMutation = () =>
-  useMutation({
+export const useSignupMutation = () => {
+  const navigate = useNavigate();
+
+  return useMutation({
     mutationFn: authAPI.signup,
     onSuccess: (data) => {
       localStorage.setItem('token', data.token);
+      navigate('/', { replace: true });
     },
   });
+};
