70f5cb4b0a5581b0acf5b91170d973b03ee7966d
sandwe <preengus@gmail.com>
2023-02-26 11:29:48 +0900

    feat: 각 Route 감싸서 Route 분리 #9

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -7,19 +7,26 @@ import TodoCreatePage from '../pages/TodoCreatePage';
 import TodoDetailPage from '../pages/TodoDetailPage';
 import TodoEditPage from '../pages/TodoEditPage';
 import Navbar from '../components/common/Navbar';
+import PrivateRoutes from './PrivateRoutes';
+import RestrictedRoutes from './RestrictedRoutes';
 
 const Router = () => {
   return (
     <Routes>
       <Route path='/'>
-        <Route element={<Navbar />}>
-          <Route index element={<MainPage />} />
-          <Route path='/todos/:id' element={<TodoDetailPage />} />
-          <Route path='/new' element={<TodoCreatePage />} />
-          <Route path='/edit/:id' element={<TodoEditPage />} />
+        <Route element={<PrivateRoutes />}>
+          <Route element={<Navbar />}>
+            <Route index element={<MainPage />} />
+            <Route path='/todos/:id' element={<TodoDetailPage />} />
+            <Route path='/new' element={<TodoCreatePage />} />
+            <Route path='/edit/:id' element={<TodoEditPage />} />
+          </Route>
         </Route>
-        <Route path='auth' element={<LoginPage />} />
-        <Route path='signup' element={<SignupPage />} />
+
+        <Route element={<RestrictedRoutes />}>
+          <Route path='auth' element={<LoginPage />} />
+          <Route path='signup' element={<SignupPage />} />
+        </Route>
       </Route>
 
       <Route path='*' element={<NotFoundPage />} />
