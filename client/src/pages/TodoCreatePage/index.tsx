cfdfc736c9d992ac79741a02e812dd1eaa287a5c
sandwe <preengus@gmail.com>
2023-03-02 22:06:28 +0900

    refactor: TodoCreatePage 컴포넌트 내 props object 생성 및 props type 정의  #10

--- client/src/pages/TodoCreatePage/index.tsx
+++ client/src/pages/TodoCreatePage/index.tsx
@@ -1,13 +1,38 @@
+import { useState, useRef } from 'react';
+
 import TodoLayout from '../../components/todos/TodoLayout';
 import TodoForm from '../../components/todos/TodoForm';
 import { usePostTodoMutation } from '../../queries/todo';
+import { VAC } from 'react-vac';
+
+export interface TodoFormProps {
+  contentRef: React.RefObject<HTMLTextAreaElement | null>;
+  handleChangeTitle: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleChangeContent: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  handleSubmit: (e: React.FormEvent) => void;
+}
 
 const TodoCreatePage = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const contentRef = useRef<HTMLTextAreaElement>(null);
   const { mutate } = usePostTodoMutation();
+
+  const todoFormProps: TodoFormProps = {
+    contentRef,
+    handleChangeTitle: (e) => setTitle(e.target.value),
+    handleChangeContent: (e) => setContent(e.target.value),
+    handleSubmit: (e) => {
+      e.preventDefault();
+      mutate({ title, content });
+    },
+  };
 
   return (
     <TodoLayout>
-      <TodoForm mutate={mutate} />
+      <VAC name='view' data={todoFormProps} />
+      {/* <TodoForm mutate={mutate} /> */}
     </TodoLayout>
   );
 };
