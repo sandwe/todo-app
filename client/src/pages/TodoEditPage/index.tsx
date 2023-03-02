fadc752e31bea526d5e0d74875924a962c38aca7
sandwe <preengus@gmail.com>
2023-03-03 07:55:37 +0900

    refactor: TodoEditPage 컴포넌트 내 props object 생성 및 props type 정의 #10

--- client/src/pages/TodoEditPage/index.tsx
+++ client/src/pages/TodoEditPage/index.tsx
@@ -1,18 +1,35 @@
+import { useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 
 import TodoLayout from '../../components/todos/TodoLayout';
 import TodoForm from '../../components/todos/TodoForm';
 import { useGetTodoByIdQuery, useUpdateTodoMutation } from '../../queries/todo';
-
+import { TodoFormProps } from '../TodoCreatePage';
+import { VAC } from 'react-vac';
 const TodoEditPage = () => {
   const params = useParams();
 
-  const { data: todo } = useGetTodoByIdQuery(params?.id).data?.data;
+  const { data: todo } = useGetTodoByIdQuery(params?.id);
   const { mutate } = useUpdateTodoMutation();
+  const contentRef = useRef<HTMLTextAreaElement>(null);
+
+  const [title, setTitle] = useState(todo.title);
+  const [content, setContent] = useState(todo.content);
+
+  const todoFormProps: TodoFormProps = {
+    contentRef,
+    handleChangeTitle: (e) => setTitle(e.target.value),
+    handleChangeContent: (e) => setContent(e.target.value),
+    handleSubmit: (e) => {
+      e.preventDefault();
+      mutate({ id: params?.id, data: { title, content } });
+    },
+  };
 
   return (
     <TodoLayout>
-      <TodoForm todo={todo} mutate={mutate} isEditMode={true} />
+      <VAC name='view' data={todoFormProps} />
+      {/* <TodoForm todo={todo} mutate={mutate} isEditMode={true} /> */}
     </TodoLayout>
   );
 };
