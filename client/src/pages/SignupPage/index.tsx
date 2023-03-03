3aa2aa93c77b5fdceaa3f5ebdd07f1e10ef0821c
sandwe <preengus@gmail.com>
2023-03-03 11:45:12 +0900

    refactor: SignupPage 컴포넌트 내 props object 생성 및 props type 정의 #10

--- client/src/pages/SignupPage/index.tsx
+++ client/src/pages/SignupPage/index.tsx
@@ -1,3 +1,29 @@
-const SignupPage = () => {};
+import { useState } from 'react';
+import { validateEmail, validatePassword } from '../../utils/validator';
+import AuthFormView from '../../components/auth/AuthFormView';
+
+import { AuthFormProps } from '../LoginPage';
+import { useSignupMutation } from '../../queries/auth';
+
+const SignupPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { mutate } = useSignupMutation();
+
+  const authFormProps: AuthFormProps = {
+    type: '로그인',
+    isEmailValid: validateEmail(email),
+    isPasswordValid: validatePassword(password),
+    handleChangeEmail: (e) => setEmail(e.target.value),
+    handleChangePassword: (e) => setPassword(e.target.value),
+    handleSubmit: (e) => {
+      e.preventDefault();
+      mutate({ email, password });
+    },
+  };
+
+  return <AuthFormView {...authFormProps} />;
+};
 
 export default SignupPage;
